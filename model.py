d595cd3c1fe6169e46c9f6a0d874857775eef8c8
Chaitya <chaityadobariya02@gmail.com>
2023-09-30 17:47:39 +0530

    Initial Commit

--- model.py
+++ model.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+# from sklearn.preprocessing import StandardScaler
 import pandas as pd
 
 categories = ["name", "screen_name", "statuses_count", "followers_count", "friends_count", "favourites_count", "listed_count", "geo_enabled", "profile_use_background_image", "profile_background_tile"]
 
 data = pd.read_csv('dataset.csv')
-scaler = StandardScaler()
-scaler.fit(data[categories])
+# scaler = StandardScaler()
+# scaler.fit(data[categories])
 model = pickle.load(open("model.pkl", "rb"))
 
 app = Flask(__name__)
@@ -19,9 +19,9 @@ def hello_world():
     input_data = request.json
     input = np.array([input_data[x] for x in categories])
     input = input.reshape(1, -1)
-    input = scaler.transform(input)
+    # input = scaler.transform(input)
     print(input)
-    prediction = model.predict(input)
+    prediction = model.predict([[input]])
     print(prediction, type(prediction))
 
     return jsonify({"prediction": prediction.tolist() })
